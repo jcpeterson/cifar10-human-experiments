ae8fb1fd52271fdeaaf85ab04e134ae079e21981
Ruairidh M. Battleday <ruairidh@tigergpu.princeton.edu>
2018-09-22 15:04:48 -0400

    added resume option to a few run files

--- pytorch_image_classification/run_files/run_pyt_outer_resnext.sh
+++ pytorch_image_classification/run_files/run_pyt_outer_resnext.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 echo 'outer script'
-
+epoch=82
+model_file='/tigress/ruairidh/model_results/resnext_29_8x64d'
 time=4310
 model=resnext_29_8x64d
 log_file=${model}.out
 out_dir=${model}
-python_args="--arch=resnext --depth 29 --cardinality=8 --base_channels=64 --batch_size=64 --base_lr=0.05"
+python_args="--arch=resnext --depth 29 --cardinality=8 --base_channels=64 --batch_size=64 --base_lr=0.05 --resume=${model_file}/model_state_{epoch}.pth"
 sbatch --time=${time} --output=${log_file} --export=out_dir=${out_dir},python_args="${python_args}",log_file=$log_file run_pyt_inner.sh
--- pytorch_image_classification/run_files/run_pyt_outer_wrn_cut.sh
+++ pytorch_image_classification/run_files/run_pyt_outer_wrn_cut.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 echo 'outer script'
-
+model_file='/tigress/ruairidh/model_results/wrn_28_10_cutout16'
+epoch=112
 time=4310
 model=wrn_28_10_cutout16
 log_file=${model}.out
 out_dir=${model}
-python_args="--arch=wrn --depth 28 --epochs=200 --scheduler=cosine --base_lr=0.1 --batch_size=64"
+python_args="--arch=wrn --depth 28 --epochs=200 --scheduler=cosine --base_lr=0.1 --batch_size=64" # --resume=${model_file}/model_state_${epoch}.pth"
 sbatch --time=${time} --output=${log_file} --export=out_dir=${out_dir},python_args="${python_args}",log_file=$log_file run_pyt_inner.sh
