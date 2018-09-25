9cc53a1436e4e3edcb8bfaae89a3f1a9b5b74a17
Ruairidh M. Battleday <ruairidh@tigergpu.princeton.edu>
2018-09-25 10:43:37 -0400

    added bash files again

---
+++ pytorch_image_classification/run_files/tune_bash_inner.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+#SBATCH -p all
+#SBATCH -N 1 # node count
+#SBATCH --ntasks-per-node=1
+#SBATCH --gres=gpu:1
+#SBATCH --mem=16000
+#SBATCH --time=59
+
+
+echo 'entering inner script'
+echo 'activating virtual env'
+# activate virtual environment here
+source activate pytorch_env
+
+SDIR='/tigress/ruairidh/model_results'
+echo ${model}
+echo ${python_args}
+echo ${logfile}
+echo 'entering python script'
+resume="${SDIR}/run_1/${model}/model_best_state.pth"
+SV_DIR="${SDIR}/run_1/saves/${model}"
+echo ${resume}
+echo ${SV_DIR}
+
+
+python -u ./tune_with_cifar10h.py ${python_args} --resume=${resume} --c10h_scores_outdir=${SV_DIR}
+
+cp ./${logfile} ${SDIR}/run_1/.
+
+echo 'inner done'
+
