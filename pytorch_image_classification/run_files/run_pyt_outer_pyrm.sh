c369833459524ae95af4bcceb688c87baf324b72
Ruairidh M. Battleday <ruairidh@tigergpu.princeton.edu>
2018-09-25 23:32:58 -0400

    run

--- pytorch_image_classification/run_files/run_pyt_outer_pyrm.sh
+++ pytorch_image_classification/run_files/run_pyt_outer_pyrm.sh
@@ -6,5 +6,5 @@ time=4310
 model=pyramidnet_basic_110_270
 log_file=${model}.out
 out_dir=${model}
-python_args="--arch=pyramidnet --depth 110 --block_type=basic --pyramid_alpha=270 --resume=${model_file}/model_state_${epoch}.pth"
+python_args="--arch=pyramidnet --depth 110 --block_type=basic --pyramid_alpha=270" #--resume=${model_file}/model_state_${epoch}.pth"
 sbatch --time=${time} --output=${log_file} --export=out_dir=${out_dir},python_args="${python_args}",log_file=$log_file run_pyt_inner.sh
--- pytorch_image_classification/run_files/run_pyt_outer_resnext.sh
+++ pytorch_image_classification/run_files/run_pyt_outer_resnext.sh
@@ -6,5 +6,5 @@ time=4310
 model=resnext_29_8x64d
 log_file=${model}.out
 out_dir=${model}
-python_args="--arch=resnext --depth 29 --cardinality=8 --base_channels=64 --batch_size=64 --base_lr=0.05 --resume=${model_file}/model_state_{epoch}.pth"
+python_args="--arch=resnext --depth 29 --cardinality=8 --base_channels=64 --batch_size=64 --base_lr=0.05" # --resume=${model_file}/model_state_{epoch}.pth"
 sbatch --time=${time} --output=${log_file} --export=out_dir=${out_dir},python_args="${python_args}",log_file=$log_file run_pyt_inner.sh
