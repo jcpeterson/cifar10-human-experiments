33882d59031a74d678b5d2e6379952a605c7215c
Joshua Peterson <peterson.c.joshua@gmail.com>
2018-11-13 21:27:37 -0500

    better csv checker

--- pytorch_image_classification/check_csvs.py
+++ pytorch_image_classification/check_csvs.py
@@ -13,9 +13,6 @@ out_path = getattr(args, 'path')
 def list_folders_only(path):
     return [x for x in listdir(path) if isdir(join(path, x))]
 
-# assume true and try to prove false
-epochs_consistent = True
-
 model_folders = sorted(list_folders_only(out_path))
 for model_folder in model_folders:
     param_folders = list_folders_only(join(out_path, model_folder))
@@ -23,22 +20,33 @@ for model_folder in model_folders:
 
     print(model_folder)
     print('(contains', len(param_folders), 'folders)')
-    
+
+    # assume true and try to prove false
+    epochs_consistent = True
+    epochs_mod_10 = True
+
     for param_folder in param_folders:
-        df = pd.read_csv(join(out_path, 
-                              model_folder, 
-                              param_folder, 
+        df = pd.read_csv(join(out_path,
+                              model_folder,
+                              param_folder,
                               'scores.csv'))
         try:
             if df.epoch.max() != last_epoch:
                 epochs_consistent = False
             else:
-                last_epoch = df.epoch.max() 
+                last_epoch = df.epoch.max()
         except:
             last_epoch = df.epoch.max()
+        if df.epoch.max() % 10 != 0:
+            epochs_mod_10 = False
         print('   ', param_folder, 'EPOCHS DONE:', df.epoch.max())
 
-if not epochs_consistent: 
-    print("WARNING: NOT ALL MODELS HAVE SAME # OF EPOCHS!!!")
-else:
-    print("SUCCESS: ALL MODELS HAVE SAME # OF EPOCHS!")
+    if not epochs_consistent:
+        print('    WARNING: NOT ALL MODELS HAVE SAME # OF EPOCHS')
+    else:
+        print('    SUCCESS: ALL MODELS HAVE SAME # OF EPOCHS')
+    if not epochs_mod_10:
+        print('    WARNING: MODEL SET PROBABLY NOT DONE (epochs finished not a multiple of 10)')
+    else:
+        print('    SUCCESS: IT APPEARS THAT THIS MODEL SET FINISHED')
+    print('')
