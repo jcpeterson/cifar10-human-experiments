5ce285cd4658cee63f438fd8d001f27fdc60cc98
battleday_69 <ruairidh.battleday@gmail.com>
2019-03-14 13:50:22 -0400

    beginning

---
+++ predictions/get_val_idxs.py
@@ -0,0 +1,17 @@
+import os, sys
+import numpy as np
+
+script, seed = sys.argv
+seed = int(seed)
+perc = 0.1
+data_len = 10000
+c10h_rnd_idxs = np.arange(data_len)
+
+# this seed is the same for self.set in ['train','test']
+np.random.seed(seed)
+np.random.shuffle(c10h_rnd_idxs)
+split_idx = int((1 - perc)*data_len)
+final = c10h_rnd_idxs[split_idx:]
+print(final[:5])
+np.save('./val_idx_seed_{0}'.format(str(seed)), final)
+
