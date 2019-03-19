cc011c729c039be32e7ed1110de6aa21d9fe530b
Ruairidh M. Battleday <ruairidh@tigergpu.princeton.edu>
2019-03-19 11:28:56 -0400

    added copy opt fn

---
+++ analysis/copy_opt_folder.py
@@ -0,0 +1,19 @@
+import os, sys, shutil
+
+def copy_optimal_dir(params, in_dir = '/tigress/ruairidh/model_results/9k_tuning_run_1', out_dir = '/tigress/ruairidh/model_results/optimal_9k'):
+    """Takes the params of the optimal model and copys the source 
+    dir to a nice format in the optimal folder.
+    Params: model, control, lr, seed, fold"""
+
+    model, control, lr, seed, fold = params
+    identifier = 'con_{0}_lr_{1}_seed_{2}_fold_{3}'.format(control, lr, seed, fold)
+    in_path = os.path.join(in_dir, model, identifier)
+    print(in_path)
+    out_path = os.path.join(out_dir, control, model)
+    print(out_path)
+    params_path = os.path.join(out_dir, control, '{0}_{1}'.format(model, identifier))
+    print(params_path)
+    shutil.copytree(in_path, out_path)
+    open('{0}.txt'.format(params_path), 'a').close() 
+    
+copy_optimal_dir(['resnet_basic_110', 'False', '0.1', '0', '0'])
