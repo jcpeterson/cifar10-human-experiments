10d25283a59cfc9ad8c8141ff834a33c71c6ab7a
Ruairidh M. Battleday <ruairidh@tigergpu.princeton.edu>
2019-03-21 21:41:41 -0400

    prediction wrappers

--- pytorch_image_classification/PREDICTION_code/bash_pred_extract_outer.sh
+++ pytorch_image_classification/PREDICTION_code/bash_pred_extract_outer.sh
@@ -2,12 +2,12 @@
 
 # activate conda environment
 models='vgg_15_BN_64
+resnet_basic_110
 resnet_preact_bottleneck_164
 densenet_BC_100_12
 wrn_28_10
 pyramidnet_basic_110_270
 resnext_29_8x64d
-wrn_28_10_cutout16
 shake_shake_26_2x64d_SSI_cutout16' # no commas!
 # maybe no quotes needed below?
 
